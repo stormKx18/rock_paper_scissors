2ec58703e871c04649f1e0dfb04ead9a7432d241
hirian <vicjaime922@gmail.com>
2024-05-21 18:55:20 -0600

    Final changes

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ console.log('Rock Paper Scissors game');
 
 let humanScore=0;
 let computerScore=0;
+let gamesPlayed=0;
 
 //getComputerChoice will randomly return 
 //one of the following string values: “rock”, “paper” or “scissors”
@@ -11,31 +12,82 @@ function getComputerChoice(){
     return rnd;
 }
 
-function updateScores(){
+function updateScores(humanSel,computerSel){
+    document.querySelector("#compSel").innerText=decodeRPS(computerSel);
+    document.querySelector("#humSel").innerText=decodeRPS(humanSel);
+    //Human won
+    if(humanSel==1 && computerSel==3){
+        console.log('Human won');
+        humanScore++;
+    }else if(humanSel==2 && computerSel==1){
+        console.log('Human won');
+        humanScore++;
+    }else if(humanSel==3 && computerSel==2){
+        console.log('Human won');
+        humanScore++;
+    }
+    //Computer won
+    else if(humanSel==1 && computerSel==2){
+        console.log('Computer won');
+        computerScore++;
+    }else if(humanSel==2 && computerSel==3){
+        console.log('Computer won');
+        computerScore++;
+    }else if(humanSel==3 && computerSel==1){
+        console.log('Computer won');
+        computerScore++;
+    }else{
+        console.log('Draw');
+    }
+
     document.querySelector("#hscore").innerText=humanScore;
     document.querySelector("#cscore").innerText=computerScore;
 }
 
 function getHumanChoice(){
-    let ans = prompt('Select 1(Rock), 2(Paper) or 3(Scissors)')
+    let ans = +prompt('Select 1(Rock), 2(Paper) or 3(Scissors)')
+    ans = Math.min(ans,3);
     return ans;
 }
 
 function playRound(){
     let humanSel=getHumanChoice();
     let computerSel=getComputerChoice();
-    
+
+    updateScores(humanSel,computerSel);
+    gamesPlayed++;
+
+}
+
+function playGame(){
+    document.querySelector("#winner").innerText='';
+
+    playRound();  
+
+    if(gamesPlayed>=5){
+        if (computerScore>humanScore){
+            document.querySelector("#winner").innerText='Computer won!';
+        }else if(computerScore<humanScore){
+            document.querySelector("#winner").innerText='Human won!';
+        }else{
+            document.querySelector("#winner").innerText='It is a draw!';
+        }
+    }
+
 }
 
 
 function decodeRPS(rnd){
     if (rnd==1){
-        console.log('Rock');
+        return('Rock')
+        //console.log('Rock');
     }
     else if(rnd==2){
-        console.log('Paper');
+        return('Paper')
+        //console.log('Paper');
     }
     else{
-        console.log('Scissors');
+        return('Scissors')
+        //console.log('Scissors');
     }
 }
