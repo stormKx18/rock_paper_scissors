d00bfb260c963aaa824b08c35c4f3a49699930ee
hirian <vicjaime922@gmail.com>
2024-05-21 18:22:27 -0600

    Added button to start game

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
 console.log('Rock Paper Scissors game');
+
+let humanScore=0;
+let computerScore=0;
+
+//getComputerChoice will randomly return 
+//one of the following string values: “rock”, “paper” or “scissors”
+
+function getComputerChoice(){
+    let rnd = Math.floor(Math.random()*3)+1;
+    decodeRPS(rnd);
+    
+    return rnd;
+}
+
+function getHumanChoice(){
+    let ans = prompt('Select 1(Rock), 2(Paper) or 3(Scissors)')
+    decodeRPS(ans);
+
+    document.querySelector("#hscore").innerText=humanScore;
+    document.querySelector("#cscore").innerText=computerScore;
+}
+
+
+function decodeRPS(rnd){
+    if (rnd==1){
+        console.log('Rock');
+    }
+    else if(rnd==2){
+        console.log('Paper');
+    }
+    else{
+        console.log('Scissors');
+    }
+}
